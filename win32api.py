f154ac4e6f344898836ce34a37a4508e1a3604ce
nemoisthebest <167675622+nemoisthebest@users.noreply.github.com>
2024-06-17 15:30:13 +0300

    Update win32api.py

--- win32api.py
+++ win32api.py
@@ -1,9 +1,37 @@
+# ייבוא מודלים הנחוצים לתוכנית
 import win32api
 import time
+import random
 
-def click(x,y):
-    win32api.SetCursorPos((x,y))
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
-    time.sleep(0.10)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
-    time.sleep(0.10) 
+# הגדרת זמן ריצה
+runtime = 1 * 60
+
+# הגדרת פונקציה 
+def click():
+   
+# הגדרת זמן לריצת התוכנית וזמן לעצירתה
+   start_time = time.time()
+
+   while True:
+        current_time = time.time()
+        elapsed_time = current_time - start_time
+
+        if elapsed_time > runtime:
+            break
+
+# הלולאה שבה העכבר זז כל עשר שניות למקום רנדומלי שהתוכנית גינרטה ומדפיסה את המיקום של העכבר במסך
+        for i in range(0,1):
+            screen_width = win32api.GetSystemMetrics(0)
+            screen_height = win32api.GetSystemMetrics(1)
+
+            x = random.randint(0,screen_width)
+            y = random.randint(0,screen_height)
+
+            win32api.SetCursorPos((x,y))
+
+            print(f"mouse moved to: ({x},{y})")
+
+            time.sleep(10)
+
+# קריאה לפונקציה
+click()
