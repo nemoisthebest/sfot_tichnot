03877b6fdcbdbb2af4e1d4c9866d6dfeb498f166
nemoisthebest <167675622+nemoisthebest@users.noreply.github.com>
2024-06-18 14:48:29 +0300

    Update socket.py

--- socket.py
+++ socket.py
@@ -1,7 +1,7 @@
 # Name: 1-socket.py
 # Author: emily h. (nemo) 
 # Date: 30.04.24
-# Description: 
+# Description: A program that connects via socket to a remote connection, receives CMD commands and returns output, and on the other side connects with netcat.
 
 import socket 
 host = ""
