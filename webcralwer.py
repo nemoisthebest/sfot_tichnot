bf659e63af64e5a49f3b01d3f7a558b874d868d5
nemoisthebest <167675622+nemoisthebest@users.noreply.github.com>
2024-06-19 14:59:31 +0300

    Update webcralwer.py

--- webcralwer.py
+++ webcralwer.py
@@ -3,12 +3,28 @@
 # Date: 30.04.24
 # Description: webcralwer that receives a file with common file names on websites and tries to access them all.
 
+# ייבוא מודל
 import requests
-from bs4 import BeautifulSoup
-url = "https://example.com"
-response = requests.get(url)
 
-for link in url:
-    href = link.get("href")
-    if href.startswith("http"):
-        response = requests.get(href)
+#הגדרת משתנים
+def main():
+website = input ("Enter the website you want to do a web crawler on: ")
+urls_file_path = input ("Enter the file path: ")
+urls_file = open(urls_file_path, "r")
+current_url = urls_file.readline()[:-1]
+
+#הגדרת לולאה שבה הפונקציה תרוץ עד שיגמרו המחרוזות בקובץ
+while len(current_url) != 0:
+ 
+ # מעבר על שורות הקובץ לאחר שהבקשה הפכה לדף דפדפן והדפסת הכתובות שנמצאות באתר  
+    response = requests.get(website + current_url)
+    if response.status_code in range(200, 299):
+        print(response.url)
+    current_url = urls_file.readline()[:-1]
+
+# סגירת קריאת הקובץ
+urls_file.close()
+
+# קריאה לפונקציה
+if __name__ == "__main__":
+    main()
