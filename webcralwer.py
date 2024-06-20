18494fa11f59c07d4d1d55dd7cdd45770d5d99a2
nemoisthebest <167675622+nemoisthebest@users.noreply.github.com>
2024-06-20 23:14:15 +0300

    Update webcralwer.py

--- webcralwer.py
+++ webcralwer.py
@@ -3,40 +3,28 @@
 # Date: 19.06.24
 # Description: webcralwer that receives a file with common file names on websites and tries to access them all.
 
-# ייבוא מודלים
-import socket 
-import sys 
-import subprocess
+# ייבוא מודל
+import requests
 
-# הגדרת חיבור סוקט ווידוא הצלחתו
-try: 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-    print ("Socket successfully created")
-except socket.error as err: 
-    print ("socket creation failed with error %s" %(err))
-
-# הגדרת פורט עליו תיוווצר ההתחברות
-port = 80
-
-# הגדרת כתובת המקור
-host_ip = "127.0.0.1"  
-
-# הגדרת העמדה כמקשיב למחשב השני שרוצה להתחבר אליו
-s.bind((host_ip, port))
-s.listen(5)
-
-# הקמת התקשורת בין העמדות
-c, addr = s.accept()     
-print ('Got connection from', addr)
-
+#הגדרת משתנים
 def main():
-    while True: 
-        # קבלת פקודה, הרצתה והחזרת הפלט לעמדה שאליה התחברנו
-        command = c.recv(1024).decode()
-        output = subprocess.run([command], shell=True, capture_output=True, text=True)
-        output = output.stdout.encode()
-        c.send(output)
-        
+website = input ("Enter the website you want to do a web crawler on: ")
+urls_file_path = input ("Enter the file path: ")
+urls_file = open(urls_file_path, "r")
+current_url = urls_file.readline()[:-1]
+
+#הגדרת לולאה שבה הפונקציה תרוץ עד שיגמרו המחרוזות בקובץ
+while len(current_url) != 0:
+ 
+ # מעבר על שורות הקובץ לאחר שהבקשה הפכה לדף דפדפן והדפסת הכתובות שנמצאות באתר  
+    response = requests.get(website + current_url)
+    if response.status_code in range(200, 299):
+        print(response.url)
+    current_url = urls_file.readline()[:-1]
+
+# סגירת קריאת הקובץ
+urls_file.close()
+
+# קריאה לפונקציה
 if __name__ == "__main__":
     main()
-
