4e091336f10451563988d3c807ccd9f3c67bafe3
nemoisthebest <167675622+nemoisthebest@users.noreply.github.com>
2024-06-20 19:23:39 +0300

    Update socket.py

--- socket.py
+++ socket.py
@@ -1,27 +1,5 @@
 # Name: 1-socket.py
 # Author: emily h. (nemo) 
-# Date: 30.04.24
+# Date: 20.06.24
 # Description: A program that connects via socket to a remote connection, receives CMD commands and returns output, and on the other side connects with netcat.
 
-import socket 
-host = ""
-port = ""
-
-import socket
-hostname = ""
-portnum = ""
-
-def netcat ():
-	 s=socket.socket()
-
-#create socket and connect the clinet
-with socket.socket () as client:
-    client.connect((host, port))
-
-
-#recive commands
-commands = client.recv ()
-
-#send answer
-answer = input ("")
-client.sendall (answer)
