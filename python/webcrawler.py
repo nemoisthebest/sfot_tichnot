190dd8f5d097cec6f019a19e59a7e33f170460cc
nemoisthebest <167675622+nemoisthebest@users.noreply.github.com>
2024-07-30 09:51:40 +0300

    Update webcrawler.py

--- python/webcrawler.py
+++ python/webcrawler.py
@@ -8,22 +8,22 @@ import requests
 
 #הגדרת משתנים
 def main():
-website = input ("Enter the website you want to do a web crawler on: ")
-urls_file_path = input ("Enter the file path: ")
-urls_file = open(urls_file_path, "r")
-current_url = urls_file.readline()[:-1]
-
-#הגדרת לולאה שבה הפונקציה תרוץ עד שיגמרו המחרוזות בקובץ
-while len(current_url) != 0:
- 
- # מעבר על שורות הקובץ לאחר שהבקשה הפכה לדף דפדפן והדפסת הכתובות שנמצאות באתר  
-    response = requests.get(website + current_url)
-    if response.status_code in range(200, 299):
-        print(response.url)
+    website = input ("Enter the website you want to do a web crawler on: ")
+    urls_file_path = input ("Enter the file path: ")
+    urls_file = open(urls_file_path, "r")
     current_url = urls_file.readline()[:-1]
 
+#הגדרת לולאה שבה הפונקציה תרוץ עד שיגמרו המחרוזות בקובץ
+    while len(current_url) != 0:
+ 
+ # מעבר על שורות הקובץ לאחר שהבקשה הפכה לדף דפדפן והדפסת הכתובות שנמצאות באתר  
+        response = requests.get(website + current_url)
+        if response.status_code in range(200, 299):
+            print(response.url)
+        current_url = urls_file.readline()[:-1]
+
 # סגירת קריאת הקובץ
-urls_file.close()
+    urls_file.close()
 
 # קריאה לפונקציה
 if __name__ == "__main__":
